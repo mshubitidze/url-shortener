ee272593450a8a95b62603340fc161b32979a502
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2022-12-03 02:17:35 +0400

    rollback

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith("/api/get-url")) {
+  const url = req.nextUrl.clone();
+  console.log("url clone >>>>", url);
+  if (
+    url.pathname.length === 9 &&
+    url.pathname.startsWith("/") &&
+    !url.pathname.includes("api")
+  ) {
     const slug = req.nextUrl.pathname.split("/").pop();
 
     const jsonData = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
