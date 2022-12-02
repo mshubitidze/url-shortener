a50f60f1e992fcbd22d9d7c7ad29612ce890af4f
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2022-12-03 02:03:16 +0400

    fix middleware checking every get request

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,23 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+export async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith("/api/get-url")) {
-    // console.log("returning early");
-
     return;
   }
+  const url = req.nextUrl.clone();
+  if (
+    url.pathname.length === 9 &&
+    url.pathname.startsWith("/") &&
+    !url.pathname.includes("api")
+  ) {
+    const slug = req.nextUrl.pathname.split("/").pop();
 
-  // console.log("Path??", req.nextUrl.pathname);
+    const jsonData = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
 
-  const slug = req.nextUrl.pathname.split("/").pop();
+    const data = typeof jsonData === "object" ? await jsonData.json() : null;
 
-  // console.log("full next url", req.nextUrl);
-  //
-  const jsonData = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
-
-  const data = typeof jsonData === "object" ? await jsonData.json() : null;
-
-  if (data?.url) {
-    return NextResponse.redirect(data.url);
+    if (data?.url) {
+      return NextResponse.redirect(data.url);
+    }
   }
-
-  // console.log("data =>>>", data);
 }
