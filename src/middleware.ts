5013d5d18a59697826c459d3fc08d0a6e1a9d5f7
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2022-12-03 01:46:49 +0400

    previous commit fix except actually :)

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,27 +4,22 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (req.nextUrl.pathname.startsWith("/api/get-url")) {
     // console.log("returning early");
 
-    // console.log("Path??", req.nextUrl.pathname);
-
-    if (req.nextUrl.pathname.startsWith("/api/get-url")) {
-      const slug = req.nextUrl.pathname.split("/").pop();
-
-      // console.log("full next url", req.nextUrl);
-
-      const data = await (
-        await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
-      ).json();
-
-      if (data?.url) {
-        return NextResponse.redirect(data.url);
-      }
-    }
     return;
   }
 
-  // other urls
-  console.log("returning everything other than api url requests");
-  return;
+  // console.log("Path??", req.nextUrl.pathname);
+
+  const slug = req.nextUrl.pathname.split("/").pop();
+
+  // console.log("full next url", req.nextUrl);
+  //
+  const jsonData = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+
+  const data = typeof jsonData === "object" ? await jsonData.json() : null;
+
+  if (data?.url) {
+    return NextResponse.redirect(data.url);
+  }
 
   // console.log("data =>>>", data);
 }
