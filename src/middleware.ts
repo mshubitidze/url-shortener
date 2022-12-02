38e685191526d1f3e16ea412eadcdcedae4bd059
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2022-12-03 02:11:27 +0400

    changed middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith("/api/get-url")) {
-    return;
-  }
-  const url = req.nextUrl.clone();
-  if (
-    url.pathname.length === 9 &&
-    url.pathname.startsWith("/") &&
-    !url.pathname.includes("api")
-  ) {
     const slug = req.nextUrl.pathname.split("/").pop();
 
     const jsonData = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
